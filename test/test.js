89aa0472764a88de60bc85c7d5ac32c1629aca52
shinnn <snnskwtnb@gmail.com>
2014-02-28 20:51:39 +0900

    update test files and scripts

--- test/test.js
+++ test/test.js
@@ -3,11 +3,12 @@
 var assert = require('assert');
 var fs = require('fs');
 
-var expected = fs.readFileSync('test/expected.css');
+var expected = fs.readFileSync('test/expected/multiple_mediaqueries.css');
 
 describe('broccoli-css-mqpacker', function () {
   it('should run css-mqpacker', function (done) {
-    fs.readFile('test/actual/fixture.css', function(err, data) {
+    fs.readFile('test/actual/multiple_mediaqueries.css', function(err, data) {
+      if (err) done(err);
       assert.strictEqual(data.toString(), expected.toString());
       done();
     });
